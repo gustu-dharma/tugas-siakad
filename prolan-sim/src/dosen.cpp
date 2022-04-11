d549868661618ef0c65db29eca0288c7e1b09569
gustu-dharma <100062138+gustu-dharma@users.noreply.github.com>
2022-04-11 23:37:17 +0700

    Add files via upload

---
+++ prolan-sim/src/dosen.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+#include "include/dosen.hpp"
+
+
+dosen::dosen(std::string id, std::string nama, int dd, int mm, int yy, std::string npp, std::string departemen, std::string pendidikan, std::string fakultas)
+		: person(id, nama, dd, mm, yy), npp(npp), departemen(departemen), pendidikan(pendidikan), fakultas(fakultas)
+{
+}
+
+void dosen::setPendidikan(std::string pendidikan)
+{
+	this->pendidikan = pendidikan;
+}
+
+std::string dosen::getPendidikan()
+{
+	return this->pendidikan;
+}
+void dosen::setNPP(std::string npp)
+{
+	this->npp =npp;
+}
+
+std::string dosen::getNPP()
+{
+	return this->npp;
+}
+
+void dosen::setDepartemen(std::string departemen)
+{
+	this->departemen = departemen;
+}
+
+std::string dosen::getDepartemen()
+{
+	return this->departemen;
+}
+
+void dosen::setFakultas(std::string departemen)
+{
+	this->fakultas = fakultas;
+}
+
+std::string dosen::getFakultas()
+{
+	return this->fakultas;
+}
+
